ac12847057d284f70a255c7e5854e34c3b522f8d
Eric Garner <ericgarner81@gmail.com>
2023-10-01 22:16:45 -0500

    completed exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,17 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a  == 17 && b == 4) 
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
         }
     }
 }
